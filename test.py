958a7a31ebc8b8ac9861f40ae0161e1832a4d1a0
Anna <sowaamk@gmail.com>
2020-11-15 19:54:50 +0100

    Name correction

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import math
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def minus(a, b):
+    print(a)
     return a - b
+    
 
 def division(a, b):
     return a/b
 
 print(add(2, 2))
-print(subtract(3, 2))
+print(minus(3, 2))
 print(division(4, 2)
