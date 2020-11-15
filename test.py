df4406c4ee045c66ddccc966f2a79cad971136f3
Anna <sowaamk@gmail.com>
2020-11-15 19:33:47 +0100

    Kopia pierwszego pliku

---
+++ test.py
@@ -0,0 +1,18 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def subtract(a, b):
+    return a/b
+
+def multiply(a, b):
+    return a * b
+
+print(add(2, 2))
+print(minus(3, 2))
+print(subtract(4, 2))
+print(multiply(2, 2))
